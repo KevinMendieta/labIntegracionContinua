84be13f2b4c5946e9335b78886b6626f682df3a7
KevinMendieta <kevin.mendieta-p@mail.esucelaing.edu.co>
2016-10-13 11:15:36 -0500

    Correnccion ParteII y clase pruebas

---
+++ src/test/java/EntradasForoTest.java
@@ -0,0 +1,28 @@
+
+import org.junit.Before;
+import org.junit.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 2118677
+ */
+public class EntradasForoTest{
+    public EntradasForoTest(){
+    }
+    
+    @Before
+    public void setUp(){
+        
+    }
+    
+    @Test
+    public void entradasForoSinComentarios(){
+        
+    }
+}
